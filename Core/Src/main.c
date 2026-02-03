105c5c74c41dd078a22520df840509b0b4912e52
feihu_wang <2214164070@qq.com>
2026-02-03 15:01:23 +0800

    regenerate usb device CDC class

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,7 +20,7 @@
 #include "main.h"
 #include "i2c.h"
 #include "usart.h"
-#include "usb_otg.h"
+#include "usb_device.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -95,7 +95,7 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_USART1_UART_Init();
-  MX_USB_OTG_HS_HCD_Init();
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
